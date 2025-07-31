0dc6218dfa3ec3ce85ab5e9a5c89fced5dd47949
Bhinsar Jagdish <bhinsarjagdish2003@gmail.com>
2025-07-31 13:07:22 +0530

    fix routes path

--- backend/index.js
+++ backend/index.js
@@ -2,27 +2,25 @@ const express = require("express");
 const dotenv = require("dotenv");
 const cookieParser = require("cookie-parser");
 const bodyParser = require("body-parser");
-const fs = require("fs");
 const path = require("path");
 const { readdirSync } = require("fs");
+
 dotenv.config();
 
-// Connect to MongoDB
 require("./src/utils/connection");
 
 const app = express();
 
-const port = process.env.PORT || 8080;
-
 app.use(bodyParser.json());
 app.use(cookieParser());
 
-readdirSync("./src/routes").map((r) => app.use("/", require("./src/routes/" + r)));
+const routesPath = path.join(__dirname, "src/routes");
+readdirSync(routesPath).map((r) =>
+  app.use("/", require(path.join(routesPath, r)))
+);
 
 app.get("/running", (req, res) => {
   res.send("Server is running");
 });
 
-app.listen(port, () => {
-  console.log(`Server running on port ${port}`);
-});
+module.exports = app;
